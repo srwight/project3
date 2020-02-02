98473b09e0a7e185a499e04267292fe9baec712e
srwight <srwight@gmail.com>
2020-02-02 12:35:26 -0500

    New Base with Tests

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,15 @@
-def Hello():
-  return 'Hello World'
-  
+def func(a:int) -> int:
+    """[summary]
+    
+    Arguments:
+        a {int} -- [description]
+    
+    Returns:
+        int,int -- [description]
+    """ 
+        
+    return a+1
+
+
+def test_answer():
+    assert func(3) == 5
