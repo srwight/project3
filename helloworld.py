9781fb5c126d2e8b88fd2315c81785d74b60094d
srwight <srwight@gmail.com>
2020-02-02 23:00:46 -0500

    to make jenkins fire

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,11 @@
 def func(a:int) -> int:
-    """Returns an integer a + 1
+    """Returns an integer a + 2
     
     Arguments:
         a {int} -- any integer
     
     Returns:
-        int -- a + 1
+        int -- a + 2
     """ 
         
-    return a+1
+    return a+2
